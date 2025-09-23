e031d6b3f499f958428dc30ac1ab72db1bba1bef
stephanjames <stephanjames055@gmail.com>
2025-09-23 21:04:59 +0000

    updated backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,14 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-states"         
-    key            = "infra/ecr/terraform.tfstate"   
-    region         = "eu-north-1"                  
-    workspace_key_prefix = "env:"
-    use_lockfile   = true
+  # bucket         = "my-terraform-states"  
+   bucket         = "my-terraform-states-api-deploy" 
+   key            = "infra/ecr/terraform.tfstate"   
+   region         = "eu-north-1"                  
+   # workspace_key_prefix = "env:"
+   # use_lockfile   = true
+   dynamodb_table = "terraform-locks"
+   encrypt        = true
+
+   
   }
 }
