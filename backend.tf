d4729df1820a8515a8cbb053c752d4de0c1a6859
stephanjames <stephanjames055@gmail.com>
2025-09-24 00:29:20 +0000

    updated the backend bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
   # bucket         = "my-terraform-states"  
-   bucket         = "my-terraform-states-api-deploy-2" 
+   bucket         = "my-terraform-states-api-deploy-3" 
    key            = "infra/ecr/terraform.tfstate"   
    region         = "eu-north-1"                  
    # workspace_key_prefix = "env:"
